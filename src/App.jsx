7c7cf6c9dacfb577eac33cf8d884ef803f244d4c
mayday555tomato <35573264+mayday555tomato@users.noreply.github.com>
2018-01-18 17:20:29 -0500

    chapter 3
    using react properties 
    dynamic properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,90 @@
 const contentNode = document.getElementById('contents');
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europa'];
-const message = continents.map(c=> `Hello ${c}!`).join(' ');
-const component = <p>{message}</p>;
-ReactDOM.render(component, contentNode);
+
+const issues = [
+    {
+        id: 1, status: 'Open', owner: 'Ravan', 
+        created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+        title: 'Error in console when clicking Add'
+    },
+    {
+        id: 2, status: 'Assigned', owner: 'Eddie',
+        created: new Date('2016-08-16'), effort: 12, completionDate: new Date('2016-08-30'),
+        title: 'Missing bottom border on panel'
+    }
+];
+
+class IssueFilter extends React.Component{
+    render(){
+        return (
+            <div>This is a placeholder for the issue filter.</div>
+        );
+    }
+}
+
+class IssueTable extends React.Component{
+    render(){
+        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
+        return (
+            <table className="bordered-table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Status</th>
+                        <th>Owner</th>
+                        <th>Created</th>
+                        <th>Effort</th>
+                        <th>Completion Date</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {issueRows}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+class IssueRow extends React.Component{
+    render(){
+        const issue = this.props.issue;
+        //using this.props.childer to pass in a formatted HTML content directly to the IssueRow 
+        return (
+            <tr>
+                <td>{issue.id}</td>
+                <td>{issue.status}</td>
+                <td>{issue.owner}</td>
+                <td>{issue.created.toDateString()}</td>
+                <td>{issue.effort}</td>
+                <td>{issue.completionDate? issue.completionDate.toDateString() : ''}</td>
+                <td>{issue.title}</td>
+            </tr>
+        )
+    }
+}
+
+class IssueAdd extends React.Component{
+    render(){
+        return (
+            <div>This is a placeholder for the issue add.</div>
+        );
+    }
+}
+class IssueList extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Issue List</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable issues={issues}/>
+                <hr />
+                <IssueAdd />
+            </div>
+        );
+    }
+}
+
+
+
+ReactDOM.render(<IssueList />, contentNode);
