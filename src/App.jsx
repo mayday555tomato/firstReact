d3fa66bf424ab6623b1eb99536a901bb75546ed9
mayday555tomato <35573264+mayday555tomato@users.noreply.github.com>
2018-01-20 14:11:55 -0500

    chapter 04 react states

---
+++ src/App.jsx
@@ -0,0 +1,141 @@
+const contentNode = document.getElementById('contents');
+
+const issues = [
+    {
+        id: 1, status: 'Open', owner: 'Ravan', 
+        created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+        title: 'Error in console when clicking Add'
+    },
+    {
+        id: 2, status: 'Assigned', owner: 'Eddie',
+        created: new Date('2016-08-16'), effort: 12, completionDate: new Date('2016-08-30'),
+        title: 'Missing bottom border on panel'
+    }
+];
+
+class IssueFilter extends React.Component{
+    render(){
+        return (
+            <div>This is a placeholder for the issue filter.</div>
+        );
+    }
+}
+
+function IssueTable(props){
+    const issueRows  = props.issues.map(issue =><IssueRow key={issue.id} issue = {issue} />);
+    return (
+        <table className="bordered-table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Status</th>
+                        <th>Owner</th>
+                        <th>Created</th>
+                        <th>Effort</th>
+                        <th>Completion Date</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {issueRows}
+                </tbody>
+            </table>
+    );
+}
+
+function IssueRow(props){
+    return (
+        <tr>
+        <td>{props.issue.id}</td>
+        <td>{props.issue.status}</td>
+        <td>{props.issue.owner}</td>
+        <td>{props.issue.created.toDateString()}</td>
+        <td>{props.issue.effort}</td>
+        <td>{props.issue.completionDate? props.issue.completionDate.toDateString() : ''}</td>
+        <td>{props.issue.title}</td>
+    </tr>
+    )
+}
+/*
+const IssueRow = (props) => (
+    <tr>
+        <td>{props.issue.id}</td>
+        <td>{props.issue.status}</td>
+        <td>{props.issue.owner}</td>
+        <td>{props.issue.created.toDateString()}</td>
+        <td>{props.issue.effort}</td>
+        <td>{props.issue.completionDate? props.issue.completionDate.toDateString() : ''}</td>
+        <td>{props.issue.title}</td>
+    </tr>
+)*/
+
+class IssueAdd extends React.Component{
+    constructor(){
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        var form = document.forms.issueAdd;
+        this.props.createIssue({
+            owner: form.owner.value,
+            title: form.title.value,
+            status: 'New',
+            created: new Date(),
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <form name='issueAdd' onSubmit={this.handleSubmit}>
+                    <input type='text' name ='owner' placeholder = 'Owner' />
+                    <input type = 'text' name = 'title' placeholder = 'Title' />
+                    <button>Add</button>
+                </form>
+            </div>
+        );
+    }
+}
+class IssueList extends React.Component {
+    constructor(){
+        super();
+        this.state = {issues : [], };
+        this.createIssue = this.createIssue.bind(this);
+    }
+
+    componentDidMount(){
+        this.loadData();
+    }
+
+    loadData(){
+        setTimeout(()=>{
+            this.setState({issues: issues,});
+        }, 2000);
+        
+    }
+
+    createIssue(newIssue){
+        const newIssues = this.state.issues.slice();
+        newIssue.id = newIssues.length + 1;
+        newIssues.push(newIssue);
+        this.setState({issues: newIssues});
+    }
+    render(){
+        return (
+            <div>
+                <h1>Issue List</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable issues={this.state.issues} />
+                <hr />
+                <IssueAdd createIssue={this.createIssue} />
+            </div>
+        );
+    }
+}
+
+
+
+ReactDOM.render(<IssueList />, contentNode);
