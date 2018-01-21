c01253ba8dc24e191623f345194a0ef174df6e8c
mayday555tomato <35573264+mayday555tomato@users.noreply.github.com>
2018-01-21 14:29:18 -0500

    06 using mongo db

---
+++ server.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+app.use(express.static('static'));
+app.use(bodyParser.json());
+/*
+const issues = [
+    {
+        id: 1, status: 'Open', owner: 'Ravan', 
+        created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+        title: 'Error in console when clicking Add'
+    },
+    {
+        id: 2, status: 'Assigned', owner: 'Eddie',
+        created: new Date('2016-08-16'), effort: 12, completionDate: new Date('2016-08-30'),
+        title: 'Missing bottom border on panel'
+    }
+];
+*/
+
+
+let db;
+MongoClient.connect('mongodb://localhost/issuetracker')
+           .then(connection => {
+               db = connection.db('issuetracker'); //need 
+               app.listen(3000, ()=> {
+                   console.log('App started on port 3000');
+               });
+           }).catch(error => {
+               console.log('ERROR:', error);
+           });
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/issues', (req, res)=>{
+    db.collection('issues').find().toArray()
+      .then(issues => {
+          const metadata = {total_count: issues.length};          
+          res.json({_metadata: metadata, records: issues})
+      }).catch(error => {
+          console.log(error);
+          res.status(500).json({message: `Internal Server Error: ${error}`});
+      });
+    console.log('Get /api/issues is called...');
+});
+
+
+app.post('/api/issues', (req, res) => {
+    console.log('Post /api/issues is called...');
+    const newIssue = req.body; //from List createIssue fetch 'body: JSON.stringify(newIssue)'
+    newIssue.created = new Date();
+    if (!newIssue.status)
+        newIssue.status = 'New';
+    const err = validateIssue(newIssue);
+    if (err){
+        res.status(422).json({message: `Invalid requrest: ${err}`});
+        return;
+    }
+    db.collection('issues').insertOne(newIssue).then(result =>
+        db.collection('issues').find({_id: result.insertedId}).limit(1).next()).then(newIssue =>{
+            res.json(newIssue);
+        }).catch(error => {
+            console.log(error);
+            res.status(500).json({message: `Internal Server Error: ${error}`});
+        });
+});
+
+const validIssueStatus = {
+    New : true,
+    Open: true,
+    Assigned: true,
+    Fixed: true,
+    Verified: true,
+    Closed: true,
+};
+
+const issueFieldType = {
+    status: 'required',
+    owner: 'required',
+    effort: 'optional',
+    created: 'required',
+    completionDate: 'optional',
+    title: 'required',
+};
+
+function validateIssue(issue){
+    for(const field in issue){
+        const type = issueFieldType[field];
+        if (!type){
+            delete issue[field];
+        }else if (type === 'required' && !issue[field]){
+            return `${field} is required.`;
+        }
+    }
+
+    if (!validIssueStatus[issue.status])
+        return `${issue.status} is not a vlid status.`;
+    
+    return null;
+}
