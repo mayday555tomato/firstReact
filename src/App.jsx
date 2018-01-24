7785645a669c334e54243f7a519cc11b9344e583
mayday555tomato <35573264+mayday555tomato@users.noreply.github.com>
2018-01-23 20:40:51 -0500

    08 Simple route
    use react-router-dom instead of react-router in App.jsx (but you still need to install react-router
    HashRouter will route based on hash tags #
    HashRouter can only have one child. 
    - if use <div> it will render all the route pages in one single page
    - so use <Switch> it will only render one page that matches the url
    localhost:3000 hit enter becomes localhost:3000/#/ 
    localhost:3000/#/ddd will open issueEdit page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import {HashRouter as Router, Route, Link, Switch} from 'react-router-dom';
+
 import IssueList from './IssueList.jsx';
+import IssueEdit from './IssueEdit.jsx';
 
 const contentNode = document.getElementById('contents');
+const NoMatch = () => <p> Page Not Found </p>;
 
-ReactDOM.render(<IssueList />, contentNode);
+const RoutedApp = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={IssueList}/>
+            <Route path="/ddd" component={IssueEdit}/>
+            <Route path="*" component={NoMatch}/>
+        </Switch>
+    </Router>
+  );
+  
+ReactDOM.render(<RoutedApp />, contentNode);
 
 if (module.hot) {
     module.hot.accept();
