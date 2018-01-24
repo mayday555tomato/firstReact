c28d1a61f894b4fcf74de5c5f78385216798db8a
mayday555tomato <35573264+mayday555tomato@users.noreply.github.com>
2018-01-23 20:41:41 -0500

    08 simple routes
    updated webpack.config.js file because of new installation: react-router and react-router-dom

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 module.exports = {
     entry: {
         app: ['./src/App.jsx'],
-        vendor: ['react', 'react-dom','whatwg-fetch','babel-polyfill'],
+        vendor: ['react', 'react-dom', 'whatwg-fetch', 'babel-polyfill', 'react-router-dom','react-router'],
     },
 
     output: {
